a86381852d41bed17fcb6f4efdd7ec2d07b56545
Ferid-M <117301474+Ferid-M@users.noreply.github.com>
2022-11-21 12:37:28 +0400

    Add files via upload
    Add files

---
+++ Task Calculator/Calculator/Calculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace method_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Birinci ededi daxil edin :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Goreceyiniz Emeliyyati secin :");
+            char opr = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Ikinci ededi daxil edin :");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (opr=='+' || opr =='-' || opr == '*' || opr == '/' )
+            {
+                Console.Clear();
+
+                Console.WriteLine("Cavab : " + " " + calculator(num1, num2, opr));
+            }
+            else
+            {
+                Console.WriteLine("Duzgun operatoru daxil edin");
+            }
+
+            
+
+        }
+        static int calculator(int num1, int num2, char opr)
+        {
+            int result = 0;
+            switch (opr)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+            return result;
+
+
+        }
+
+    }
+}
